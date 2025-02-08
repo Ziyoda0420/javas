523da0ce21f6298b4cd827a762f0e75a9efc4378
Ziyoda0420 <mirzakirovaziyoda@gmail.com>
2025-02-08 16:43:28 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const name = prompt('Ismingiz nima?');
+const age = prompt('Yoshingiz nechchida?');
+
+
+
+document.querySelector('#name').textContent = name;
+document.querySelector('#age').textContent = age;
+
+
+const resultElement = document.querySelector('#result');
+
+
